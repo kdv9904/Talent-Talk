e80b8c7787c14cf83a484c21dc7c1debf4609235
Kirtan9904 <kirtanvyas9904@gmail.com>
2026-03-06 10:33:26 +0530

    fix import

--- frontend/src/pages/SessionPage.jsx
+++ frontend/src/pages/SessionPage.jsx
@@ -26,7 +26,7 @@ import { useBotDetection } from "../hooks/useBotDetection";
 import useStreamClient from "../hooks/useStreamClient";
 import { StreamCall, StreamVideo } from "@stream-io/video-react-sdk";
 import VideoCallUI from "../components/VideoCallUI";
-import AIFeedbackModal from "../components/AIfeedbackModal";
+import AIFeedbackModal from "../components/AifeedbackModal";
 import { sessionApi } from "../api/sessions";
 
 function SessionPage() {
